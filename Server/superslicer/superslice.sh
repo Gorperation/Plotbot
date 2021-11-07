21aac9a6e49854a773498e93ae24b4d2db2ad700
Plotbot Server <github@jeremyjacob.dev>
2021-11-07 03:54:52 +0000

    Body msg log

--- Server/superslicer/superslice.sh
+++ Server/superslicer/superslice.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 cd /home/slicer/Plotbot/Server/superslicer
-<<<<<<< HEAD
-<<<<<<< HEAD
-./superslicer -g --load slicer-config.ini $SARGS ./model.3mf
-=======
 ./superslicer -g --load slicer-config.ini $SARGS
->>>>>>> 18316a194ffcdc56dae3d9f11c9713952700e1b2
-=======
-./superslicer -g --load slicer-config.ini $SARGS
->>>>>>> 18316a194ffcdc56dae3d9f11c9713952700e1b2
